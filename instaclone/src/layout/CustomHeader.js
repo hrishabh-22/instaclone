adaf39a1a684388338db2fc39aebd68b5116120f
hrishabh <hrishabhm22@gmail.com>
2021-09-24 14:27:08 +0530

    Some More UI files created

---
+++ instaclone/src/layout/CustomHeader.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {StyleSheet} from 'react-native';
+import {Text} from 'native-base';
+
+const CustomHeader = () => {
+  return (
+    <>
+      <Text>Hello from CustomHeader.js</Text>
+    </>
+  );
+};
+
+export default CustomHeader;
