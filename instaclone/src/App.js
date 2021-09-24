36bc4bd4d8370f048b3a814c784db7a5d40daa32
hrishabh <hrishabhm22@gmail.com>
2021-09-24 22:44:03 +0530

    MidWay App Completed

--- instaclone/src/App.js
+++ instaclone/src/App.js
@@ -1,23 +1,89 @@
-import React from 'react';
-import {StyleSheet} from 'react-native';
-import {Text} from 'native-base';
-import Home from './Screens/Home';
+import React, {useEffect} from 'react';
+import {StyleSheet, Text} from 'react-native';
+import 'react-native-gesture-handler';
+import auth from '@react-native-firebase/auth';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {connect, useDispatch} from 'react-redux';
 import AddPost from './Screens/AddPost';
+import Home from './Screens/Home';
 import SignIn from './Screens/SignIn';
 import SignUp from './Screens/SignUp';
+import CustomHeader from './layout/CustomHeader';
+import {SET_USER, IS_AUTHENTICATED} from './action/action.types';
+import database from '@react-native-firebase/database';
+import EmptyContainer from './components/EmptyContainer';
+import {requestPermission} from './utils/AskPermission';
 
-const App = () => {
+const Stack = createStackNavigator();
+
+const App = ({authState}) => {
+  const dispatch = useDispatch();
+
+  const onAuthStateChanged = user => {
+    if (user) {
+      dispatch({
+        type: IS_AUTHENTICATED,
+        payload: true,
+      });
+
+      console.log(user._user.uid);
+
+      database()
+        .ref(`/users/${user._user.uid}`)
+        .on('value', snapshot => {
+          console.log('USER DETAILS: ', snapshot.val());
+          dispatch({
+            type: SET_USER,
+            payload: snapshot.val(),
+          });
+        });
+    } else {
+      dispatch({
+        type: IS_AUTHENTICATED,
+        payload: false,
+      });
+    }
+  };
+
+  useEffect(() => {
+    requestPermission();
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber;
+  }, []);
+
+  if (authState.loading) {
+    return <EmptyContainer />;
+  }
+
   return (
     <>
-      <Text>Hello from app.js</Text>
-      <Home />
-      <AddPost />
-      <SignIn />
-      <SignUp />
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+            header: props => <CustomHeader {...props} />,
+          }}>
+          {authState.IS_AUTHENTICATED ? (
+            <>
+              <Stack.Screen name="Home" component={Home} />
+              <Stack.Screen name="AddPost" component={AddPost} />
+            </>
+          ) : (
+            <>
+              <Stack.Screen name="SignIn" component={SignIn} />
+              <Stack.Screen name="SignUp" component={SignUp} />
+            </>
+          )}
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 };
 
-export default App;
+const mapStateToProps = state => ({
+  authState: state.auth,
+});
+
+export default connect(mapStateToProps)(App);
 
 const styles = StyleSheet.create({});
--- instaclone/src/layout/CustomHeader.js
+++ instaclone/src/layout/CustomHeader.js
@@ -1,13 +1,48 @@
 import React from 'react';
 import {StyleSheet} from 'react-native';
-import {Text} from 'native-base';
+import {Body, Header, Right, Button, Icon, Title, Text} from 'native-base';
+import {connect} from 'react-redux';
+import propTypes from 'prop-types';
+import {signOut} from '../action/auth';
 
-const CustomHeader = () => {
+const CustomHeader = ({signOut, authState, navigation}) => {
   return (
-    <>
-      <Text>Hello from CustomHeader.js</Text>
-    </>
+    <Header
+      androidStatusBarColor="#0f4c75"
+      style={{backgroundColor: '#0f4c75'}}>
+      <Body>
+        <Title>Social App</Title>
+      </Body>
+      <Right>
+        {authState.isAuthenticated && (
+          <>
+            <Button
+              transparent
+              iconLeft
+              onPress={() => navigation.navigate('AddPost')}>
+              <Text style={{color: '#fdcb9e'}}>Add Post</Text>
+            </Button>
+            <Button transparent onPress={() => signOut}>
+              <Icon name="log-out-outline" style={{color: 'red'}} />
+            </Button>
+          </>
+        )}
+      </Right>
+    </Header>
   );
 };
 
-export default CustomHeader;
+const mapStateToProps = state => ({
+  authState: state.auth,
+});
+
+const mapDispatchToProps = {
+  signOut,
+};
+
+CustomHeader.propTypes = {
+  signOut: propTypes.func.isRequired,
+  authState: propTypes.object.isRequired,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(CustomHeader);
