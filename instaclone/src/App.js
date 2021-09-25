173d0c7ec6b3c1f75e678c1443b6188ba76d6087
hrishabh <hrishabhm22@gmail.com>
2021-09-26 00:06:43 +0530

    SignIn UI Done

--- instaclone/src/App.js
+++ instaclone/src/App.js
@@ -82,7 +82,7 @@ const App =({authState}) => {
               </>
             ) : (
               <>
-              <Stack.Screen name="SignIn" component={SignUp} />
+              <Stack.Screen name="SignIn" component={SignIn} />
               <Stack.Screen name="SignUp" component={SignUp} />
               </>
             )}
